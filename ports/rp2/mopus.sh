ce0a4537769d9bcc90e1ad8c0cb41dfcffe11c6e
Daniel Holth <dholth@fastmail.fm>
2023-02-21 14:38:39 -0500

    use PICO board

--- ports/rp2/mopus.sh
+++ ports/rp2/mopus.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-make BOARD=ADAFRUIT_QTPY_RP2040 USER_C_MODULES=../../examples/opusmodule/micropython.cmake CFLAGS_EXTRA=-DMODULE_OPUS_ENABLED=1 V=1
+make -j BOARD=PICO USER_C_MODULES=../../examples/opusmodule/micropython.cmake CFLAGS_EXTRA=-DMODULE_OPUS_ENABLED=1 V=1
