2d4858b2f2188cb31f20a9e05c4bb0724c1ff15f
Daniel Holth <dholth@fastmail.fm>
2023-02-25 11:16:50 -0500

    add some debugging info; reformat

--- examples/opusmodule/opus/oggzmodule.c
+++ examples/opusmodule/opus/oggzmodule.c
@@ -5,111 +5,122 @@
 #include <oggz/oggz.h>
 #include <opus/opus.h>
 
-typedef struct _mp_obj_oggz_t
-{
-    mp_obj_base_t base;
-    mp_obj_t stream; // retain a reference to prevent GC from reclaiming it
-    OGGZ *oggz;
-    OpusDecoder *opus;
-    oggz_packet *current_packet; // NULL unless currently decoding opus
+typedef struct _mp_obj_oggz_t {
+  mp_obj_base_t base;
+  mp_obj_t stream; // retain a reference to prevent GC from reclaiming it
+  OGGZ *oggz;
+  OpusDecoder *opus;
+  oggz_packet *current_packet; // NULL unless currently decoding opus
+  const char *content_type;
 } mp_obj_oggz_t;
 
-STATIC int oggz_packet_cb(OGGZ *oggz, oggz_packet *packet, long serialno, void *user_data)
-{
-    mp_obj_oggz_t *self = (mp_obj_oggz_t *)user_data;
-    self->current_packet = packet;
-    return OGGZ_STOP_OK;
+STATIC int oggz_packet_cb(OGGZ *oggz, oggz_packet *packet, long serialno,
+                          void *user_data) {
+  mp_obj_oggz_t *self = (mp_obj_oggz_t *)user_data;
+  self->current_packet = packet;
+  self->content_type = oggz_stream_get_content_type(oggz, serialno);
+  return OGGZ_STOP_OK;
 }
 
-STATIC mp_obj_oggz_t *mp_oggz_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw, const mp_obj_t *args)
-{
-    mp_arg_check_num(n_args, n_kw, 1, 1, false);
-    mp_obj_oggz_t *self = m_new_obj(mp_obj_oggz_t);
+STATIC mp_obj_oggz_t *mp_oggz_make_new(const mp_obj_type_t *type, size_t n_args,
+                                       size_t n_kw, const mp_obj_t *args) {
+  mp_arg_check_num(n_args, n_kw, 1, 1, false);
+  mp_obj_oggz_t *self = m_new_obj(mp_obj_oggz_t);
 
-    // make sure we have a stream
-    mp_get_stream_raise(args[0], MP_STREAM_OP_READ);
+  // make sure we have a stream
+  mp_get_stream_raise(args[0], MP_STREAM_OP_READ);
 
-    mp_obj_t *stream = args[0];
+  mp_obj_t *stream = args[0];
 
-    self->base.type = (mp_obj_type_t *)type;
-    self->stream = stream;
+  self->base.type = (mp_obj_type_t *)type;
+  self->stream = stream;
 
-    self->oggz = oggz_new(OGGZ_READ);
-    if (self->oggz == NULL)
-    {
-        mp_raise_msg_varg(&mp_type_RuntimeError, MP_ERROR_TEXT("oggz error"));
-    }
+  self->oggz = oggz_new(OGGZ_READ);
+  if (self->oggz == NULL) {
+    mp_raise_msg_varg(&mp_type_RuntimeError, MP_ERROR_TEXT("oggz error"));
+  }
 
-    int error;
+  int error;
 
-    self->opus = opus_decoder_create(48000, 2, &error);
-    if (error != 0)
-    {
-        mp_raise_msg_varg(&mp_type_RuntimeError, MP_ERROR_TEXT("opus error %d"), error);
-    }
+  self->opus = opus_decoder_create(48000, 2, &error);
+  if (error != 0) {
+    mp_raise_msg_varg(&mp_type_RuntimeError, MP_ERROR_TEXT("opus error %d"),
+                      error);
+  }
 
-    error = oggz_io_set_read(self->oggz, (OggzIORead)&mp_stream_posix_read, self->stream);
-    if (error != 0)
-    {
-        mp_raise_msg_varg(&mp_type_RuntimeError, MP_ERROR_TEXT("oggz error %d"), error);
-    }
+  error = oggz_io_set_read(self->oggz, (OggzIORead)&mp_stream_posix_read,
+                           self->stream);
+  if (error != 0) {
+    mp_raise_msg_varg(&mp_type_RuntimeError, MP_ERROR_TEXT("oggz error %d"),
+                      error);
+  }
 
-    error = oggz_set_read_callback(self->oggz, -1, oggz_packet_cb, self);
-    if (error != 0)
-    {
-        mp_raise_msg_varg(&mp_type_RuntimeError, MP_ERROR_TEXT("oggz error %d"), error);
-    }
+  error = oggz_set_read_callback(self->oggz, -1, oggz_packet_cb, self);
+  if (error != 0) {
+    mp_raise_msg_varg(&mp_type_RuntimeError, MP_ERROR_TEXT("oggz error %d"),
+                      error);
+  }
 
-    return MP_OBJ_FROM_PTR(self);
+  return MP_OBJ_FROM_PTR(self);
 }
 
-STATIC mp_obj_t mp_oggz_close(mp_obj_t self_in)
-{
-    mp_obj_oggz_t *self = MP_OBJ_TO_PTR(self_in);
-    int error = oggz_close(self->oggz);
-    self->oggz = NULL;
-    opus_decoder_destroy(self->opus);
-    self->opus = NULL;
-    if (error != 0)
-    {
-        mp_raise_msg_varg(&mp_type_RuntimeError, MP_ERROR_TEXT("oggz error %d"), error);
-    }
-    return mp_const_none;
+STATIC mp_obj_t mp_oggz_close(mp_obj_t self_in) {
+  mp_obj_oggz_t *self = MP_OBJ_TO_PTR(self_in);
+  int error = oggz_close(self->oggz);
+  self->oggz = NULL;
+  opus_decoder_destroy(self->opus);
+  self->opus = NULL;
+  if (error != 0) {
+    mp_raise_msg_varg(&mp_type_RuntimeError, MP_ERROR_TEXT("oggz error %d"),
+                      error);
+  }
+  return mp_const_none;
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_1(mp_oggz_close_obj, mp_oggz_close);
 
-STATIC mp_obj_t mp_oggz_read(mp_obj_t self_in, mp_obj_t n)
-{
-    mp_obj_oggz_t *self = MP_OBJ_TO_PTR(self_in);
-    int bytes_read = oggz_read(self->oggz, mp_obj_get_int(n));
-    if (bytes_read < 0)
-    {
-        mp_raise_msg_varg(&mp_type_RuntimeError, MP_ERROR_TEXT("oggz error %d"), bytes_read);
-    }
-    return mp_obj_new_int(bytes_read);
+STATIC mp_obj_t mp_oggz_read(mp_obj_t self_in, mp_obj_t n) {
+  mp_obj_oggz_t *self = MP_OBJ_TO_PTR(self_in);
+  int bytes_read = oggz_read(self->oggz, mp_obj_get_int(n));
+  if (bytes_read < 0 && bytes_read != OGGZ_ERR_STOP_OK) {
+    mp_raise_msg_varg(&mp_type_RuntimeError, MP_ERROR_TEXT("oggz error %d"),
+                      bytes_read);
+  }
+  return mp_obj_new_int(bytes_read);
 }
 // Define a Python reference to the function above.
 STATIC MP_DEFINE_CONST_FUN_OBJ_2(mp_oggz_read_obj, mp_oggz_read);
 
 // Decode opus samples to buffer
 // Return number of samples decoded (bytes = samples * 16 bit * 2)
-STATIC mp_obj_t mp_oggz_decode_opus(mp_obj_t self_in, mp_obj_t buf_obj)
-{
-    mp_obj_oggz_t *self = MP_OBJ_TO_PTR(self_in);
-    int bytes_read;
-    mp_buffer_info_t bufinfo;
+STATIC mp_obj_t mp_oggz_decode_opus(mp_obj_t self_in, mp_obj_t buf_obj) {
+  mp_obj_oggz_t *self = MP_OBJ_TO_PTR(self_in);
+  int bytes_read, samples_read;
+  mp_buffer_info_t bufinfo;
 
-    mp_get_buffer_raise(buf_obj, &bufinfo, MP_BUFFER_READ);
+  mp_get_buffer_raise(buf_obj, &bufinfo, MP_BUFFER_READ);
 
-    self->current_packet = NULL;
-    bytes_read = oggz_read(self->oggz, 512);
-    if (bytes_read < 0 && bytes_read != OGGZ_ERR_STOP_OK)
-    {
-        mp_raise_msg_varg(&mp_type_RuntimeError, MP_ERROR_TEXT("oggz error %d"), bytes_read);
-    }
-    int samples_read = opus_decode(self->opus, self->current_packet->op.packet, self->current_packet->op.bytes, bufinfo.buf, bufinfo.len / 4, 0);
+  self->current_packet = NULL;
+  bytes_read = oggz_read(self->oggz, 512);
+  if ((bytes_read < 0) && (bytes_read != OGGZ_ERR_STOP_OK)) {
+    mp_raise_msg_varg(&mp_type_RuntimeError, MP_ERROR_TEXT("oggz error %d"),
+                      bytes_read);
+  }
+  if (self->current_packet == NULL) {
+    samples_read = 0;
+  } else {
+    samples_read = opus_decode(self->opus, self->current_packet->op.packet,
+                               self->current_packet->op.bytes, bufinfo.buf,
+                               bufinfo.len / 4, 0);
 
-    return mp_obj_new_int(samples_read);
+    // OPUS_BAD_ARG -1, BUFFER_TOO_SMALL -2, INTERNAL_ERROR -3,
+    // INVALID_PACKET -4, UNIMPL -5, INVALID_STATE -6 ALLOC_FAIL -7
+  }
+
+  return mp_obj_new_tuple(
+      4, ((mp_obj_t[]){
+             mp_obj_new_str(self->content_type, strlen(self->content_type)),
+             mp_obj_new_int(samples_read), mp_obj_new_int(bytes_read),
+             mp_obj_new_int(bufinfo.len)}));
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_2(mp_oggz_decode_opus_obj, mp_oggz_decode_opus);
 
@@ -121,12 +132,9 @@ STATIC const mp_rom_map_elem_t oggz_locals_dict_table[] = {
 STATIC MP_DEFINE_CONST_DICT(oggz_locals_dict, oggz_locals_dict_table);
 
 // This defines the type(opus) object.
-MP_DEFINE_CONST_OBJ_TYPE(
-    mp_oggz_type,
-    MP_QSTR_oggz,
-    MP_TYPE_FLAG_NONE,
-    make_new, mp_oggz_make_new,
-    locals_dict, &oggz_locals_dict);
+MP_DEFINE_CONST_OBJ_TYPE(mp_oggz_type, MP_QSTR_oggz, MP_TYPE_FLAG_NONE,
+                         make_new, mp_oggz_make_new, locals_dict,
+                         &oggz_locals_dict);
 
 // Define all properties of the module.
 // Table entries are key/value pairs of the attribute name (a string)
--- examples/opusmodule/opus/opusmodule.c
+++ examples/opusmodule/opus/opusmodule.c
@@ -4,119 +4,108 @@
 
 #include <opusfile.h>
 
+STATIC int op_stream_posix_read(void *_stream, unsigned char *_ptr,
+                                int _nbytes);
 
-STATIC int op_stream_posix_read(void *_stream, unsigned char *_ptr, int _nbytes);
-
-typedef struct _mp_obj_opus_t
-{
-    mp_obj_base_t base;
-    mp_obj_t stream; // retain a reference to prevent GC from reclaiming it
-    OggOpusFile *of;
+typedef struct _mp_obj_opus_t {
+  mp_obj_base_t base;
+  mp_obj_t stream; // retain a reference to prevent GC from reclaiming it
+  OggOpusFile *of;
 } mp_obj_opus_t;
 
-STATIC int op_stream_posix_read(void *_stream, unsigned char *_ptr, int _nbytes)
-{
-    // cast return type to int from ssize_t
-    return (int)mp_stream_posix_read(_stream, _ptr, _nbytes);
+STATIC int op_stream_posix_read(void *_stream, unsigned char *_ptr,
+                                int _nbytes) {
+  // cast return type to int from ssize_t
+  return (int)mp_stream_posix_read(_stream, _ptr, _nbytes);
 }
 
-STATIC const OpusFileCallbacks mp_opus_callbacks = {
-    &op_stream_posix_read,
-    NULL,
-    NULL,
-    NULL};
+STATIC const OpusFileCallbacks mp_opus_callbacks = {&op_stream_posix_read, NULL,
+                                                    NULL, NULL};
 
-// Test if a file is opus, given ideally >=57 bytes from the beginnning of the file.
-STATIC mp_obj_t opus_test(mp_obj_t buf_obj)
-{
-    mp_buffer_info_t bufinfo;
-    OpusHead head;
+// Test if a file is opus, given ideally >=57 bytes from the beginnning of the
+// file.
+STATIC mp_obj_t opus_test(mp_obj_t buf_obj) {
+  mp_buffer_info_t bufinfo;
+  OpusHead head;
 
-    mp_get_buffer_raise(buf_obj, &bufinfo, MP_BUFFER_READ);
+  mp_get_buffer_raise(buf_obj, &bufinfo, MP_BUFFER_READ);
 
-    int is_opus = op_test(&head, bufinfo.buf, bufinfo.len);
-    if (is_opus < 0)
-    {
-        mp_raise_msg_varg(&mp_type_RuntimeError, MP_ERROR_TEXT("opus error %d"), is_opus);
-    }
-    return mp_const_true;
+  int is_opus = op_test(&head, bufinfo.buf, bufinfo.len);
+  if (is_opus < 0) {
+    mp_raise_msg_varg(&mp_type_RuntimeError, MP_ERROR_TEXT("opus error %d"),
+                      is_opus);
+  }
+  return mp_const_true;
 }
 // Define a Python reference to the function above.
 STATIC MP_DEFINE_CONST_FUN_OBJ_1(opus_test_obj, opus_test);
 
 // Does not close the underlying file, if any.
-STATIC mp_obj_t mp_opus_close(mp_obj_t self_in)
-{
-    mp_obj_opus_t *self = MP_OBJ_TO_PTR(self_in);
-    if (self->of == NULL)
-    {
-        mp_raise_msg_varg(&mp_type_TypeError, MP_ERROR_TEXT("opus closed"));
-    }
-    op_free(self->of);
-    self->of = NULL;
-    return mp_const_none;
+STATIC mp_obj_t mp_opus_close(mp_obj_t self_in) {
+  mp_obj_opus_t *self = MP_OBJ_TO_PTR(self_in);
+  if (self->of == NULL) {
+    mp_raise_msg_varg(&mp_type_TypeError, MP_ERROR_TEXT("opus closed"));
+  }
+  op_free(self->of);
+  self->of = NULL;
+  return mp_const_none;
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_1(mp_opus_close_obj, mp_opus_close);
 
 // Always decodes as 48khz 16-bit stereo.
-STATIC mp_obj_t mp_op_read_stereo(
-    mp_obj_t self_in,
-    mp_obj_t buf_obj)
-{
-    // "It is recommended that [the buffer] be large enough for at least 120 ms
-    // of data at 48 kHz per channel (11520 [16-bit] values total)." 48e3 *
-    // (120/1000) * 2 = 11520. i.e. 23040. bytes. (Otherwise it may just buffer
-    // extra samples internally)
+STATIC mp_obj_t mp_op_read_stereo(mp_obj_t self_in, mp_obj_t buf_obj) {
+  // "It is recommended that [the buffer] be large enough for at least 120 ms
+  // of data at 48 kHz per channel (11520 [16-bit] values total)." 48e3 *
+  // (120/1000) * 2 = 11520. i.e. 23040. bytes. (Otherwise it may just buffer
+  // extra samples internally)
 
-    // dholth: if you know you only use shorter frames, shorter buffers may be
-    // fine
+  // dholth: if you know you only use shorter frames, shorter buffers may be
+  // fine
 
-    mp_obj_opus_t *self = MP_OBJ_TO_PTR(self_in);
-    mp_buffer_info_t bufinfo;
-    mp_int_t samples_read;
+  mp_obj_opus_t *self = MP_OBJ_TO_PTR(self_in);
+  mp_buffer_info_t bufinfo;
+  mp_int_t samples_read;
 
-    mp_get_buffer_raise(buf_obj, &bufinfo, MP_BUFFER_WRITE);
+  mp_get_buffer_raise(buf_obj, &bufinfo, MP_BUFFER_WRITE);
 
-    if (self->of == NULL)
-    {
-        mp_raise_msg_varg(&mp_type_TypeError, MP_ERROR_TEXT("opus closed"));
-    }
+  if (self->of == NULL) {
+    mp_raise_msg_varg(&mp_type_TypeError, MP_ERROR_TEXT("opus closed"));
+  }
 
-    samples_read = op_read_stereo(self->of,
-                                  (opus_int16 *)bufinfo.buf,
-                                  (int)bufinfo.len);
+  samples_read =
+      op_read_stereo(self->of, (opus_int16 *)bufinfo.buf, (int)bufinfo.len);
 
-    if (samples_read < 0)
-    {
-        mp_raise_msg_varg(&mp_type_RuntimeError, MP_ERROR_TEXT("opus error %d"), samples_read);
-    }
+  if (samples_read < 0) {
+    mp_raise_msg_varg(&mp_type_RuntimeError, MP_ERROR_TEXT("opus error %d"),
+                      samples_read);
+  }
 
-    return mp_obj_new_int(samples_read);
+  return mp_obj_new_int(samples_read);
 }
 MP_DEFINE_CONST_FUN_OBJ_2(mp_op_read_stereo_obj, mp_op_read_stereo);
 
-STATIC mp_obj_opus_t *opus_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw, const mp_obj_t *args)
-{
-    mp_arg_check_num(n_args, n_kw, 1, 1, false);
-    mp_obj_opus_t *self = m_new_obj(mp_obj_opus_t);
+STATIC mp_obj_opus_t *opus_make_new(const mp_obj_type_t *type, size_t n_args,
+                                    size_t n_kw, const mp_obj_t *args) {
+  mp_arg_check_num(n_args, n_kw, 1, 1, false);
+  mp_obj_opus_t *self = m_new_obj(mp_obj_opus_t);
 
-    // make sure we have a stream
-    mp_get_stream_raise(args[0], MP_STREAM_OP_READ);
-    // could use stream pointer functions directly in opus interface?
+  // make sure we have a stream
+  mp_get_stream_raise(args[0], MP_STREAM_OP_READ);
+  // could use stream pointer functions directly in opus interface?
 
-    mp_obj_t *stream = args[0];
+  mp_obj_t *stream = args[0];
 
-    self->base.type = (mp_obj_type_t *)type;
-    self->stream = stream;
+  self->base.type = (mp_obj_type_t *)type;
+  self->stream = stream;
 
-    int error;
-    self->of = op_open_callbacks(stream, &mp_opus_callbacks, NULL, 0, &error);
-    if (error != 0)
-    {
-        mp_raise_msg_varg(&mp_type_RuntimeError, MP_ERROR_TEXT("opus error %d"), error);
-    }
+  int error;
+  self->of = op_open_callbacks(stream, &mp_opus_callbacks, NULL, 0, &error);
+  if (error != 0) {
+    mp_raise_msg_varg(&mp_type_RuntimeError, MP_ERROR_TEXT("opus error %d"),
+                      error);
+  }
 
-    return MP_OBJ_FROM_PTR(self);
+  return MP_OBJ_FROM_PTR(self);
 }
 
 // For the opusfile type
@@ -128,12 +117,9 @@ STATIC const mp_rom_map_elem_t opus_locals_dict_table[] = {
 STATIC MP_DEFINE_CONST_DICT(opus_locals_dict, opus_locals_dict_table);
 
 // This defines the type(opus) object.
-MP_DEFINE_CONST_OBJ_TYPE(
-    mp_opus_type,
-    MP_QSTR_opus,
-    MP_TYPE_FLAG_NONE,
-    make_new, opus_make_new,
-    locals_dict, &opus_locals_dict);
+MP_DEFINE_CONST_OBJ_TYPE(mp_opus_type, MP_QSTR_opus, MP_TYPE_FLAG_NONE,
+                         make_new, opus_make_new, locals_dict,
+                         &opus_locals_dict);
 
 // Define all properties of the module.
 // Table entries are key/value pairs of the attribute name (a string)
